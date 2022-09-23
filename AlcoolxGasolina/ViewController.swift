4a2b3738de81f787af16355b77d8db81633a7d91
Igor Fernandes <igormedfer@gmail.com>
2022-09-23 13:32:13 -0300

    func to calculate and format prices done.

--- AlcoolxGasolina/ViewController.swift
+++ AlcoolxGasolina/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var alcoholTextField: UITextField!
     @IBOutlet weak var gasTextField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
@@ -20,8 +20,30 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func tappedCalculate(_ sender: UIButton) {
+    // dividir o preço do litro do álcool pelo da gasolina.
+    func getValues() {
+        var difference: Double = 0
+        // unwrap optional text values AND convert to float
+        if let alcohol = alcoholTextField.text, let gas = gasTextField.text {
+            guard let alcoholPrice = Double(alcohol) else { return }
+            guard let gasPrice = Double(gas) else { return }
+            
+            difference = alcoholPrice / gasPrice
+        }
         
+        if difference > 0.7 {
+            let priceToPercent = difference * 100
+            let priceFormatted = String(format: "%.2f", priceToPercent)
+            print("compre gasolina. a diferença é de \(priceFormatted)%")
+        } else {
+            let priceToPercent = difference * 100
+            let priceFormatted = String(format: "%.2f", priceToPercent)
+            print("compre álcool. a diferença é de \(priceFormatted)%")
+        }
+    }
+    
+    @IBAction func tappedCalculate(_ sender: UIButton) {
+        getValues()
     }
 }
 
